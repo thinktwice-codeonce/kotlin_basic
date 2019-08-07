d08df83c88510e1dcb61959b708c2b0d1087a78f
phongnv10 <phongnv10@fsoft.com.vn>
2019-08-07 15:26:03 +0700

    Implement Sealed class, delegation, generic.

--- src/Main.kt
+++ src/Main.kt
@@ -1,19 +1,20 @@
 
 
 fun main() {
-    testFunctionExtension()
+    testDelegation()
 }
 
 ///-----------------------------------------------------------------------------
 fun testSwitchCase() {
     val a = 1
-    when(a) {
-        1 -> print("a == 1")
-        2 -> print("a == 2")
+    val result = when(a) {
+        1 -> println("a == 1")
+        2 -> println("a == 2")
         else -> {
-            print("a is not 1 or 2")
+            println("a is not 1 or 2")
         }
     }
+    println("switch-case result: ${result}")
 }
 
 ///-----------------------------------------------------------------------------
@@ -160,4 +161,96 @@ fun testFunctionExtension() {
     a3.printMySkill()
 
     println("Skills: " + Alien.show())
+}
+
+///-----------------------------------------------------------------------------
+//Data classes
+data class Book(val name: String, val publisher: String, var reviewScore: Int)
+
+fun testDataClass() {
+    val book = Book("Book A", "Nguyen Nhat Anh", 5)
+    println(book.toString())
+    println(book.hashCode())
+    println(book.hashCode())
+
+    book.reviewScore = 7
+    println(book.toString())
+    println(book.hashCode())
+}
+
+///-----------------------------------------------------------------------------
+//Sealed class: it's kind of extension class of enum, but it can be inherited by other classes
+sealed class MySealed {
+    class OP1: MySealed()
+    class OP2: MySealed()
+}
+
+fun testSealedClass() {
+    var mySealed: MySealed
+    mySealed = MySealed.OP2()
+    mySealed = MySealed.OP1()
+    val result = when (mySealed) {
+        is MySealed.OP1 -> "My Sealed OP1"
+        is MySealed.OP2 -> "My Sealed OP2"
+    }
+    println(result)
+}
+
+///-----------------------------------------------------------------------------
+//Test generic
+fun testGeneric1() {
+    var a: Int = 1
+    var b: Number
+    b = a
+    println(b)
+
+    var gen1 = MyGenericClass<String> ("Cool")
+    var gen2 = MyGenericClass<Any> ("Ok")
+    gen1 = gen2
+    gen2 = gen1
+}
+
+// <out t> is equal with <? extends T> in JAVA, <in t> is equal with <? super T> in JAVA
+class MyGenericClass<in T>( a: T) {
+    init {
+        println("This my generic class with value: " + a)
+    }
+}
+
+///-----------------------------------------------------------------------------
+//Test delegation
+interface Base {
+
+    val a: String
+
+    fun printMe()
+
+    fun print3()
+}
+
+class BaseImpl(val x: Int) : Base {
+
+    override val a: String = "WTH?"
+
+    override fun print3() {
+        println("Print third time!")
+    }
+
+    override fun printMe() {
+        println("Value of x: " + x)
+    }
+
+    fun print2() {
+        println("Print second time!")
+    }
+}
+
+class Derived(b: Base) : Base by b {
+
+}
+
+fun testDelegation() {
+    Derived(BaseImpl(123)).printMe()
+    Derived(BaseImpl(123)).print3()
+    println(Derived(BaseImpl(456)).a)
 }
